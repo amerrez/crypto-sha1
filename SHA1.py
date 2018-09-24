b856562a51e05cd2bc9ead8a849fc8a89fb96316
amerrez <arez@hawk.iit.edu>
2018-09-23 18:45:19 -0700

    compression

--- SHA1.py
+++ SHA1.py
@@ -12,15 +12,32 @@ def sha1(m):
     m_bin = m.encode()
     byte_len = len(m_bin)
     bit_len = byte_len * 8
+
     # appending a bit with padding
     m_bin += b'\x80'
-    # adding padding so the mesage satisfies m  = 448 (mod 512) becasue we will be adding 64 bits, the length of the original method.
-    # that length will be stored in 64 bits and it will make the total length equal to 512
+
+    # adding padding so the message
     m_bin += b'\x00' * ((56 - (byte_len + 1) % 64) % 64)
-    print(len(m_bin))
+    # the length of the message if printed will show the number of bytes and it is = (448 mod 512)
 
     # add 64 bits padding
-    print(struct.pack(b'>Q', bit_len))
+    m_bin += struct.pack(b'>Q', bit_len)
+    # the length of the mesage now is equl to (0 mod 512) it will show in bytes so it will show like (0 mod 64)
+
+
+    # break the method into 512 bit chunks (64 bytes chunks)
+    for i in range(0, len(m_bin)-64, 64):
+        chunk = m_bin[i:i+64]
+        # break each chunk to words of 4 bytes (16 words)
+        word = [0] * 80
+        for j in range(0, 64-4, 4):
+            w = chunk[j:j+4]
+            word[j:j+4] = struct.unpack(b'>I', w)[0]
+
+        # extend the 16 words to 80 words
+        for k in range(16, 80):
+            word[j] = _left_rotate(word[k - 3] ^ word[k - 8] ^ word[k - 14] ^ word[k - 16], 1)
+
 
     # after this step, the length of m_bin is = 448 mod 512
 
